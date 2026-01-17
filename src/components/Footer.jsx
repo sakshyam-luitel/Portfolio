c02f110fa4a335741645460b88f5c771b659c9fa
sakshyam-luitel <sakshyam.luitel@gmail.com>
2026-01-18 00:21:47 +0545

    mt-2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='border-t-1 border-red-600'>
-        <div className=' flex w-full md:min-h-10 items-center justify-center mt-1'>
+    <footer className='border-t-1 border-red-600 mt-2'>
+        <div className=' flex w-full md:min-h-10 items-center justify-center'>
             &copy;2026 Sakshyam Luitel
         </div>
     </footer>
