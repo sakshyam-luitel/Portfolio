e3f7e2dee155254084452a3fa9e9efa4cf67978d
sakshyam-luitel <sakshyam.luitel@gmail.com>
2026-01-17 23:07:38 +0545

    added hamburger menu for navigation bar for smaller devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,14 @@
+import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Menu , X } from "lucide-react"
+
 function Navbar(){
+    const [isVisible , setIsVisible] = useState(false)
+
+    function showDropdown(){
+        setIsVisible(!isVisible)
+    }
+
     return(<>
         <nav className="">
             <div className="flex justify-between my-2 md:my-4 mx-2 md:mx-4  border-b-1 border-red-600 items-center">
@@ -16,9 +26,19 @@ function Navbar(){
                     <li>Blogs</li>
                     <li>Contact</li>
                 </ul>
-                <div md:hidden hover:cursor-pointer>
-                    <a href="#" className="text-4xl">&#8801;</a>
+                <div>
+                    <button className="cursor-pointer text-3xl md:hidden" onClick ={showDropdown}><Menu/></button>
                 </div>
+
+                <ul className={`fixed top-0 right-0 z-999  h-dvh w-full  ${!isVisible?'hidden':'flex' } md:hidden flex-col gap-10 items-start justify-start hover:cursor-pointer backdrop-blur-xl`}>
+                    <li className="w-100% mt-2 hover:bg-gray-300 px-5 py-2  rounded-2xl " onClick ={showDropdown}><X/></li>
+                    <li className="w-100% mt-2 hover:bg-gray-300 px-5 py-2  rounded-2xl">About</li>
+                    <li className="w-100% mt-2 hover:bg-gray-300 px-5 py-2 rounded-2xl">Skills</li>
+                    <li className="w-100% mt-2 hover:bg-gray-300 px-5 py-2 rounded-2xl">Experience</li>
+                    <li className="w-100% mt-2 hover:bg-gray-300 px-5 py-2 rounded-2xl">Education</li>
+                    <li className="w-100% mt-2 hover:bg-gray-300 px-5 py-2 rounded-2xl">Blogs</li>
+                    <li className="w-100% mt-2 hover:bg-gray-300 px-5 py-2 rounded-2xl">Contact</li>
+                </ul>
             </div>
         </nav>
     </>)
