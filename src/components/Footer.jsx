9feb14ff744ff9677be94c49a1233029f5a40eed
sakshyam-luitel <sakshyam.luitel@gmail.com>
2026-01-18 18:43:58 +0545

    made card frame for showcasing project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='border-t-1 border-red-600 mt-2'>
+    <footer className='border-t-1 border-red-600 mt-2 z-50 bg-slate-950'>
         <div className=' flex w-full md:min-h-10 items-center justify-center'>
             &copy;2026 Sakshyam Luitel
         </div>
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,12 +1,19 @@
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
+import Card from "../components/Card"
 
 function ProjectsPage(){
     return(
         <>
             <Navbar/>
-            <div>This is Projects Page</div>
-            <Footer/>
+                <div className="ml-2 text-4xl text-center">Projects</div>
+                <div className="flex justify-center md:justify-between items-center absolute top-30 md:top-35 flex-wrap">
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                </div>
         </>
     )
 }
