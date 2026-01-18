0b76680ba516a2ed47da754ae1fd138f67242596
sakshyam-luitel <sakshyam.luitel@gmail.com>
2026-01-18 18:47:14 +0545

    minor change

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ function Card(){
     return(
         <>
             <div className="bg-slate-700  flex flex-col w-60 lg:w-64 h-96
-             border-1 border-purple-500 rounded-xl justify-center md:justify-start pl-4 mx-2 md:mx-4 mb-4">
+             border-1 border-purple-500 rounded-xl justify-center md:justify-start pl-4 mx-2  mb-4">
                 <div className="font-bold text-2xl">Project Name</div>
                 <div>Project Description</div>
                 <div>Redirect</div>
