99f83ae320f19c5a2708b3c5c2b3555a141d51e1
Sakshyam Luitel <sakshyam73@gmail.com>
2026-05-21 23:29:40 +0545

    Changed resume

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -15,14 +15,13 @@ function HomePage(){
                         <div><h2 className='text-slate-700 dark:text-slate-300 text-2xl md:text-3xl font-bold'>Student Of Computer Engineering</h2></div>
                         <div><p className='text-slate-600 dark:text-slate-400 text-lg md:text-xl mb-4 font-medium leading-relaxed'>Hi I am Sakshyam Luitel currently pursuing the bachelor's degree from Tribhuvan University IOE Pulchowk Campus, Kathmandu Nepal.</p></div>
                         <div className='flex justify-start gap-4 flex-wrap'>
-                            <a href='./Resume.pdf' download><div className='flex bg-indigo-600 dark:bg-indigo-500 text-white hover:bg-indigo-700 dark:hover:bg-indigo-600 transition-colors text-sm md:text-xl px-3 py-2 md:px-6 md:py-3 rounded-2xl items-center shadow-lg font-bold'>
+                            <a href='/Resume.pdf' target='_blank' rel='noopener noreferrer' download="Sakshyam_Luitel_Resume.pdf" className='flex bg-indigo-600 dark:bg-indigo-500 text-white hover:bg-indigo-700 dark:hover:bg-indigo-600 transition-colors text-sm md:text-xl px-3 py-2 md:px-6 md:py-3 rounded-2xl items-center shadow-lg font-bold'>
                                 <div className='p-0.5 hover:cursor-pointer mr-2'><Download/></div>
-                                <button className='hover:cursor-pointer'>Download Resume </button>
-                            </div></a>
-                            <Link to = "/projects"><div className='flex border-4 border-indigo-600 dark:border-indigo-400 text-indigo-600 dark:text-indigo-300 hover:bg-indigo-50 dark:hover:bg-indigo-900/30 transition-colors px-3 py-2 text-sm md:text-xl md:px-6 md:py-3 rounded-2xl items-center shadow-md font-bold'>
+                                <span className='hover:cursor-pointer'>Download Resume</span>
+                            </a>
+                            <Link to="/projects" className='flex border-4 border-indigo-600 dark:border-indigo-400 text-indigo-600 dark:text-indigo-300 hover:bg-indigo-50 dark:hover:bg-indigo-900/30 transition-colors px-3 py-2 text-sm md:text-xl md:px-6 md:py-3 rounded-2xl items-center shadow-md font-bold'>
                                 <div className='p-0.5 hover:cursor-pointer mr-2'><Eye/></div>
-                                <button className='hover:cursor-pointer'>View Projects</button>
-                            </div>
+                                <span className='hover:cursor-pointer'>View Projects</span>
                             </Link>
                             
                         </div>
